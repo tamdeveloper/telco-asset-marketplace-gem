aef843e18abd696c58b202fad2ac61543a06b690
cmanzana <carlos.manzanares+github@gmail.com>
2011-04-05 20:54:47 +0300

    more skeleton

--- lib/tam/api/sms.rb
+++ lib/tam/api/sms.rb
@@ -1,3 +1,6 @@
+require 'tam/error'
+require 'tam/user'
+
 module TAM
   class API
     # URL handler for receiving SMS
@@ -8,12 +11,24 @@ module TAM
       body = data["body"]
       access_token = data["access_token"]
       token_secret = data["token_secret"]
+      user = User.new(access_token, token_secret)
       
-      dispatch('receive_sms', access_token, token_secret, body)
+      dispatch('receive_sms', user, body)
     end
     
-    def self.send_sms(access_token, token_secret, body)
+    def self.send_sms(user, body)
       consumer = create_oauth_consumer
+      access_token = OAuth::AccessToken.new(consumer, user.access_token, user.token_secret)
+      data = JSON.generate({'body' => body})
+      response = access_token.post("/api/1/sms/send", data, {'Content-Type' => 'application/json'})
+      
+      if response.class == Net::HTTPUnauthorized
+        raise RequestNotAuthorized.new(response.message, response.body)
+      elsif response.class == Net::HTTPOK
+        return
+      else
+        raise UnexpectedError.new(response.message, response.body)
+      end
     end
   end
 end
