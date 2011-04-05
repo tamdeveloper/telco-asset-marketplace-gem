290ba2f1df5dc9f9926839015e7d16669bb542e8
cmanzana <carlos.manzanares+github@gmail.com>
2011-04-05 19:28:52 +0300

    continue with skeleton

--- lib/tam/api.rb
+++ lib/tam/api.rb
@@ -1,21 +1,9 @@
 require 'sinatra'
-require 'json'
 
 module TAM
   class API < Sinatra::Base
+    require 'tam/api/sms'
     
-    # URL handler for receiving SMS
-    post "/*/receive_sms" do
-      request.body.rewind # incase someone already read it
-      data = JSON.parse request.body.read
-      puts data
-      sms_body = data[:body]
-      access_token = data[:access_token]
-      
-      dispatch('receive_sms', access_token, sms_body)
-    end
-    
-    private
     def dispatch(method, *args)
       if !TAM.consumer_handler.nil? and TAM.consumer_handler.respond_to?(method)
         TAM.consumer_handler.send(method, *args)
