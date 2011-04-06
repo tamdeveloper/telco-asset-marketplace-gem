45f257951afad5e6a1a6b62c8395eba699325c82
cmanzana <carlos.manzanares+github@gmail.com>
2011-04-06 09:59:45 +0300

    adding some comments

--- lib/tam/api/sms.rb
+++ lib/tam/api/sms.rb
@@ -17,6 +17,7 @@ module TAM
       dispatch_to_handler('receive_sms', from_user, to_app, body)
     end
     
+    # Sends an SMS
     def self.send_sms(from_app, to_user, body)
       payload = JSON.generate({'body' => body, 'from' => from_app})
       dispatch_to_tam('/api/1/sms/send', to_user, payload)
